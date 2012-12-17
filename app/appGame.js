934b3ba741ec78cd29d4e3268ccf5c073182c29c
Amber van Os <amvanos@hotmail.com>
2012-12-17 10:24:29 +0100

    Game.html

---
+++ app/appGame.js
@@ -0,0 +1,17 @@
+﻿//Tournaments Web App *(Backbone example application)*
+/**
+ *	Tournaments Web App (Backbone example application)
+ *	
+ *
+ */
+
+// # League data #
+leagueData = [
+	{ name: "Threesome Ultimate Tournament", schedulingFormat:"regular"},
+	{ name: "Wisconsin Swiss 2012", schedulingFormat:"swiss"},
+	{ name: "New York City PoNY", schedulingFormat:"regular"},
+	{ name: "Swiss Chalet"}
+];
+
+// Create instance of master view
+var league = new LeagueView();
